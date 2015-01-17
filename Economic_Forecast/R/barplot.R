8c0b3b3a8d2aa8ebb90bab031323bc6ae0121564
RobHayward <robohay@gmail.com>
2015-01-17 10:02:26 +0000

    Updated forecast.  GDP contribution now works

--- Economic_Forecast/R/barplot.R
+++ Economic_Forecast/R/barplot.R
@@ -1,14 +1,46 @@
+library(tidyr)
+library(RColorBrewer)
+colors <- brewer.pal(n = 5, "Greens")
 dates <- (2015:2017)
 C <- c(25, 30, 40)
 G <- c(5, 5, 2)
 I <- c(25, 25, 25)
 X <- c(15, 14, 15)
 M <- c(20, 20, 1)
-da <- data.frame(dates, C, G, I, X, M)
+NX <- X-M
+NX
+da <- data.frame(dates, C, G, I, X, -M)
+spread
 dam <- as.matrix(da)
-damt <- t(dam)
+dam
+(dim(dam)[1]-1):dim(da)[1]
+dams <- dam[(dim(dam)[1]-1):dim(dam)[1],]
+dams
+damt <- t(dams)
 damt
 damt1 <- damt[-1,]
-colnames(damt1) <- dates
-barplot(damt1, beside  = TRUE, main = "Contributions to GDP growth")
-legend("topleft", legend = c("C", "G", "I", "X"), fill = )
+damt[1,]
+colnames(damt1) <- damt[1,]
+col <- c('red', 'blue', 'dark green', 'yellow', 'black')
+barplot(damt1, beside  = TRUE, main = "Contributions to GDP growth", col = colors)
+legend("topright", legend = c("C", "G", "I", "X"), fill = colors)
+#------------------
+colors4 <- brewer.pal(n = 4, "Greens")
+da1 <- data.frame(C, G, I, NX)
+da1m <- as.matrix(da1)
+da1mt <- t(da1m)
+colnames(da1mt) <- dates
+barplot(da1mt, beside = TRUE, main = "GDP components", col = colors4)
+legend("topright", legend = c("C", "G", "I", "NX"), fill = colors4)
+#---------------
+stocks <- data.frame(
+    time = as.Date('2009-01-01') + 0:9,
+    X = rnorm(10, 0, 1),
+    Y = rnorm(10, 0, 2),
+    Z = rnorm(10, 0, 4)
+  )
+stocks
+stocksm <- stocks %>% gather(stock, price, -time)
+stocksm
+stocksm %>% spread(stock, price)
+stocksm %>% spread(time, price)  
