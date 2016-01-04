49f408e0667f0fe079e78d2092ce806faad78ad5
RobHayward <robohay@gmail.com>
2016-01-04 13:49:29 +0000

    Updated more from event paper, event slides. There is likely to be a conflict with similar updated at home and work.

--- Event/R/SampleSize.R
+++ Event/R/SampleSize.R
@@ -2,6 +2,7 @@ par(mfrow = c(2, 2))
 sequence <- c(10, 100, 1000, 10000)
 df <- matrix(nrow = 4, ncol = 2)
 colnames(df) <- c("mean", "Var")
+rownames(df) <- sequence
 j = 1
 for(i in sequence){
 rand <- rnorm(i, 0, 1)
