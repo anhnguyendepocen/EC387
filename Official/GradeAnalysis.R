bcd3d9c20980a05c79bef290374399f9ffd7c9b5
RobHayward <robohay@gmail.com>
2016-02-17 17:29:06 +0000

    Sorted out the Credit Migration.  Paris works here but out of date.

--- Official/GradeAnalysis.R
+++ Official/GradeAnalysis.R
@@ -1,12 +1,21 @@
 #Grade analysis 
 da <- read.csv("Official/Marks2016.csv", stringsAsFactors = FALSE)
 head(da)
-barplot(da$Grade[order(-da$Grade)], names.arg = da$Last.Name
-        [order(-da$Grade)], las = 2, main = "EC387 Order of first 
-        Assignment", col = "lightblue")
+par(mfrow = c(2,1))
+assignments <- c("HF", "Event")
+for(i in assignments){
+barplot(da[order(-da[i]),i], names.arg = da[order(-da[i]), "Last.Name"],
+        las = 2, main = paste("EC387 Order of", i, 
+        "Assignment", sep = " "), col = "lightblue")
 # Obviously supress names for publication.
-abline(h = mean(da$Grade))
+abline(h = mean(da[,i], na.rm = TRUE))
 text(x = 23, y = 64, labels = paste("Mean Grade","=", 
-            round(mean(da$Grade), 2), sep = " "))
-hist(da$Grade, probability = TRUE,  main = "Dispursion of grades", 
-     breaks = 10, col = "lightblue")
+            round(mean(da[,i], na.rm = TRUE), 2), sep = " "))
+}
+# maybe make these the same xaxis or same bins in future.
+par(mfrow = c(2,1))
+for(i in assignments){
+hist(da[,i], probability = TRUE,  main = paste("Dispursion of grades for", i,
+     "Assignment", sep = " "), breaks = 10, col = "lightblue", xlab = "")
+}
+
