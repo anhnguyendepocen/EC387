44942bee0efed5dfb5590fd023a6b1232c6a51ad
RobHayward <robohay@gmail.com>
2016-04-07 15:48:50 +0100

    Updated EC387 results

--- Official/GradeAnalysis.R
+++ Official/GradeAnalysis.R
@@ -1,8 +1,8 @@
 #Grade analysis 
 da <- read.csv("Official/Marks2016.csv", stringsAsFactors = FALSE)
 head(da)
-par(mfrow = c(2,1))
-assignments <- c("HF", "Event")
+par(mfrow = c(2,2))
+assignments <- c("HF", "Event", "Technical")
 breaks <- c(20, 30, 40, 50, 60, 70, 80, 90, 100)
 for(i in assignments){
 barplot(da[order(-da[i]),i], names.arg = da[order(-da[i]), "Last.Name"],
@@ -14,7 +14,7 @@ text(x = 23, y = 70, labels = paste("Mean Grade","=",
             round(mean(da[,i], na.rm = TRUE), 2), sep = " "))
 }
 # maybe make these the same xaxis or same bins in future.
-par(mfrow = c(2,1))
+par(mfrow = c(2,2))
 for(i in assignments){
 hist(da[,i], probability = TRUE,  main = paste("Dispursion of grades for", i,
      "Assignment", sep = " "), breaks = breaks, col = "lightblue", xlab = "")
